8918c256b1a8859424c578d3975092da377880ba
jincito <110742524+jincito@users.noreply.github.com>
2024-09-04 11:24:19 -0400

    refactor code for cleanliness

--- app/page.js
+++ app/page.js
@@ -1,24 +1,38 @@
 "use client";
-import Image from "next/image";
 import { useState, useEffect } from "react";
-import { firestore } from "@/firebase";
 import {
   Box,
+  Stack,
+  Typography,
   Button,
   Modal,
-  Stack,
   TextField,
-  Typography,
 } from "@mui/material";
+import { firestore } from "@/firebase";
 import {
   collection,
-  deleteDoc,
   doc,
   getDocs,
   query,
-  getDoc,
   setDoc,
+  deleteDoc,
+  getDoc,
 } from "firebase/firestore";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "white",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: 3,
+};
 
 export default function Home() {
   const [inventory, setInventory] = useState([]); //state var to store inventory
@@ -28,18 +42,12 @@ export default function Home() {
   // async functions wont block code while fetching, entire website freezes while fetching
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, "inventory"));
-    //snapshot is the inventory on firebase db
     const docs = await getDocs(snapshot);
-    //fetches snapshot(inventory in DB)'s documents
     const inventoryList = [];
     docs.forEach((doc) => {
-      inventoryList.push({
-        name: doc.id,
-        ...doc.data(),
-      });
+      inventoryList.push({ name: doc.id, ...doc.data() });
     });
     setInventory(inventoryList);
-    console.log(inventoryList);
   };
 
   const addItem = async (item) => {
@@ -87,44 +95,36 @@ export default function Home() {
     <Box
       width="100vw"
       height="100vh"
-      flexDirection="column"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
+      display={"flex"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      alignItems={"center"}
       gap={2}
     >
-      <Modal open={open} onClose={handleClose}>
-        <Box
-          position="absolute"
-          top="50%"
-          left="50%"
-          width={400}
-          bgcolor="white"
-          border="2px solid #000"
-          boxShadow={24}
-          p={4}
-          display="flex" // flex centers the div/box
-          flexDirection="column"
-          gap={3}
-          sx={{
-            // material ui sx
-            transform: "translate(-50%,-50%)",
-          }}
-        >
-          <Typography variant="h6">Add Item</Typography>
-          <Stack width="100%" direction="row" spacing={2}>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width="100%" direction={"row"} spacing={2}>
             <TextField
+              id="outlined-basic"
+              label="Item"
               variant="outlined"
               fullWidth
               value={itemName}
-              onChange={(e) => {
-                setItemName(e.target.value);
-              }}
+              onChange={(e) => setItemName(e.target.value)}
             />
             <Button
               variant="outlined"
               onClick={() => {
                 addItem(itemName);
+                setItemName("");
                 handleClose();
               }}
             >
@@ -133,63 +133,43 @@ export default function Home() {
           </Stack>
         </Box>
       </Modal>
-      <Button
-        variant="contained"
-        onClick={() => {
-          handleOpen();
-        }}
-      >
+      <Button variant="contained" onClick={handleOpen}>
         Add New Item
       </Button>
-      <Box border="1px solid #333">
+      <Box border={"1px solid #333"}>
         <Box
           width="800px"
           height="100px"
-          bgcolor="#ADD8E6"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
+          bgcolor={"#ADD8E6"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
         >
-          <Typography variant="h2" color="#333">
+          <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
             Inventory Items
           </Typography>
         </Box>
-        <Stack width="800px" height="300px" spacing={2} overflow="auto">
+        <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
           {inventory.map(({ name, quantity }) => (
             <Box
               key={name}
               width="100%"
               minHeight="150px"
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              bgcolor="#f0f0f0"
-              padding={5}
+              display={"flex"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              bgcolor={"#f0f0f0"}
+              paddingX={5}
             >
-              <Typography variant="h3" color="#333" textAlign="center">
+              <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
                 {name.charAt(0).toUpperCase() + name.slice(1)}
               </Typography>
-              <Typography variant="h3" color="#333" textAlign="center">
-                {quantity}
+              <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
+                Quantity: {quantity}
               </Typography>
-              <Stack direction="row" spacing={2}>
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    addItem(name);
-                  }}
-                >
-                  Add
-                </Button>
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    removeItem(name);
-                  }}
-                >
-                  Remove
-                </Button>
-              </Stack>
+              <Button variant="contained" onClick={() => removeItem(name)}>
+                Remove
+              </Button>
             </Box>
           ))}
         </Stack>
